851d13db6c7f993fa9fff2e530f271149f9e9b5d
mira0427 <rajanurulamirah00@gmail.com>
2023-11-21 15:15:03 +0800

    update icon - mira

--- views/script/databasefx.js
+++ views/script/databasefx.js
@@ -16,14 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function setTableSize(state) {
         if (state === "expanded") {
-            table.style.marginLeft = "5%"; 
+            table.style.marginLeft = "5%";
         } else {
-            table.style.marginLeft = originalMarginLeft; 
+            table.style.marginLeft = originalMarginLeft;
         }
     }
     let sidenavLinks = document.querySelectorAll('.sidenav a');
 
-    sidenavLinks.forEach(function(link) {
+    sidenavLinks.forEach(function (link) {
         if (link.href === window.location.href) {
             link.classList.add('active');
         }
@@ -40,26 +40,26 @@ document.addEventListener("DOMContentLoaded", function () {
 function showImage(imagePath) {
     var modal = document.getElementById("imageModal");
     var modalImage = document.getElementById("modalImage");
-  
+
     modalImage.src = imagePath;
-  
+
     modal.style.display = "block";
-  }
-  
-  function closeModal() {
+}
+
+function closeModal() {
     var modal = document.getElementById("imageModal");
-  
+
     modal.style.display = "none";
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     var imageLinks = document.querySelectorAll(".image-link");
-    imageLinks.forEach(function(link) {
-      link.addEventListener("click", function(event) {
-        event.preventDefault();
-        var imagePath = link.getAttribute("href");
-        showImage(imagePath);
-      });
+    imageLinks.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            var imagePath = link.getAttribute("href");
+            showImage(imagePath);
+        });
     });
 });
 
@@ -95,6 +95,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 var icon = document.createElement('i');
                 icon.className = 'fa fa-exclamation-circle';
                 iconContainer.appendChild(icon);
+                icon.style.color = 'red';
+
+                var popupElement = document.createElement('div');
+                popupElement.className = 'custom-popup';
+                popupElement.innerHTML = 'This is a custom popup!';
+                popupElement.style.display = 'none'; 
+                iconContainer.appendChild(popupElement);
+
+                iconContainer.addEventListener('mouseenter', function () {
+                    showStatus(popupElement);
+                });
+
+                iconContainer.addEventListener('mouseleave', function () {
+                    hideStatus(popupElement);
+                });
+            }
+            else {
+                sovButton.style.backgroundColor = 'green';
+                var icon = document.createElement('i');
+                iconContainer.appendChild(icon);
             }
         }
     });
@@ -102,15 +122,36 @@ document.addEventListener('DOMContentLoaded', function () {
     complianceButtons.forEach(function (complianceButton) {
         var index = complianceButton.getAttribute('id').split('_')[1];
         var iconContainer = document.getElementById('complianceIconContainer_' + index);
-    
+
         if (complianceButton && iconContainer) {
             var maggiComplianceValue = complianceButton.getAttribute('data-maggi');
             var nestleComplianceValue = complianceButton.getAttribute('data-nestle');
             var maggiEyeValue = complianceButton.getAttribute('eye-maggi');
             var nestleEyeValue = complianceButton.getAttribute('eye-nestle');
-    
-            if (maggiComplianceValue === 'Non-Compliance' || nestleComplianceValue === 'Non-Compliance' || maggiEyeValue === 'No' || nestleEyeValue === 'No') {
+
+            if ((maggiComplianceValue === 'Non-Compliance' || nestleComplianceValue === 'Non-Compliance') && (maggiEyeValue === 'No' || nestleEyeValue === 'No')) {
                 complianceButton.style.backgroundColor = 'red';
+                var icon = document.createElement('i');
+                icon.className = 'fa fa-exclamation-circle';
+                iconContainer.appendChild(icon);
+                icon.style.color = 'red';
+
+                var popupElement = document.createElement('div');
+                popupElement.className = 'custom-popup';
+                popupElement.innerHTML = 'This is a custom popup!';
+                popupElement.style.display = 'none'; 
+                iconContainer.appendChild(popupElement);
+
+                iconContainer.addEventListener('mouseenter', function () {
+                    showStatus(popupElement);
+                });
+
+                iconContainer.addEventListener('mouseleave', function () {
+                    hideStatus(popupElement);
+                });
+            } else if ((maggiComplianceValue === 'Compliance' && nestleComplianceValue === 'Compliance') && (maggiEyeValue === 'Yes' && nestleEyeValue === 'Yes')) {
+                complianceButton.style.backgroundColor = 'green';
+            } else {
                 var icon = document.createElement('i');
                 icon.className = 'fa fa-exclamation-circle';
                 iconContainer.appendChild(icon);
@@ -121,20 +162,50 @@ document.addEventListener('DOMContentLoaded', function () {
     oosButtons.forEach(function (oosButton) {
         var index = oosButton.getAttribute('id').split('_')[1];
         var iconContainer = document.getElementById('oosIconContainer_' + index);
-    
+
         if (oosButton && iconContainer) {
             var kariValue = oosButton.getAttribute('data-kari');
             var tomyumValue = oosButton.getAttribute('data-tomyum');
             var kokoValue = oosButton.getAttribute('data-koko');
             var miloValue = oosButton.getAttribute('data-milo');
             var starValue = oosButton.getAttribute('data-star');
-    
-            if (kariValue === 'Yes' || tomyumValue === 'Yes' || kokoValue === 'Yes' || miloValue === 'Yes' || starValue === 'Yes') {
+
+            if ((kariValue === 'Yes' || tomyumValue === 'Yes') && (kokoValue === 'Yes' || miloValue === 'Yes' || starValue === 'Yes')) {
                 oosButton.style.backgroundColor = 'red';
+                var icon = document.createElement('i');
+                icon.className = 'fa fa-exclamation-circle';
+                iconContainer.appendChild(icon);
+                icon.style.color = 'red';
+
+                var popupElement = document.createElement('div');
+                popupElement.className = 'custom-popup';
+                popupElement.innerHTML = 'This is a custom popup!';
+                popupElement.style.display = 'none'; 
+                iconContainer.appendChild(popupElement);
+
+                iconContainer.addEventListener('mouseenter', function () {
+                    showStatus(popupElement);
+                });
+
+                iconContainer.addEventListener('mouseleave', function () {
+                    hideStatus(popupElement);
+                });
+            } else if ((kariValue === 'No' && tomyumValue === 'No') && (kokoValue === 'No' && miloValue === 'No' && starValue === 'No')) {
+                oosButton.style.backgroundColor = 'green';
+            }
+            else {
                 var icon = document.createElement('i');
                 icon.className = 'fa fa-exclamation-circle';
                 iconContainer.appendChild(icon);
             }
         }
     });
+
+    function showStatus(popupElement) {
+        popupElement.style.display = 'block';
+    }
+
+    function hideStatus(popupElement) {
+        popupElement.style.display = 'none';
+    }
 });
